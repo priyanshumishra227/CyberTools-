6c54415632b17775daa9ca9c9871017d39aa4646
CyberWithPriyanshu <cyberwithpriyanshu@gmail.com>
2025-11-28 21:27:12 +0530

    Access level Checker is done

---
+++ AccessLevelChecker.py
@@ -0,0 +1,10 @@
+username = input("Enter the username: ")
+
+if username == "admin":
+    print("You have the admin level access")
+
+elif username == "Manager":
+    print("You have manager level access")
+
+else :
+    print("You have user level access")
